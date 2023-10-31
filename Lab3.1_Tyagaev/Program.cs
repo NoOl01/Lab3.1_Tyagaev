b797dcaf32f3d4337685eaf6807dd08e219c3d4c
Anton <Anton@DESKTOP-6RE5PBQ>
2023-10-31 05:14:09 +0200

    Добавьте файлы проекта.

---
+++ Lab3.1_Tyagaev/Program.cs
@@ -0,0 +1,20 @@
+﻿// Lab 3.1 Номер: 12. Уровень: Средний.
+try
+{
+    Console.Write("Введите k:");
+    int k = int.Parse(Console.ReadLine());
+    double P = 1;
+    double S = 0;
+    for (int i = 1; i <= k; i++)
+    {
+        if (i != 3) S += (Math.Pow((-1), i)) / (i - 3);
+        for (int n = i; n <= 2*k; n++)
+        {
+            if (n != 2) P *= (Math.Pow(n, 3) - 8) / (n + 2);
+        }
+    }
+    Console.WriteLine($"W = {S * P:F2}");
+catch (Exception ex)
+{
+    Console.WriteLine(ex.Message);
+}
